5b10a61b4a3176051fbee5ecc6d6b480c2c6c44f
nightSanches <galesavich@gmail.com>
2026-01-31 05:59:42 +0500

    Создана страница MainWindow и логика для неё.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -11,14 +11,42 @@ using System.Windows.Shapes;
 
 namespace EquipmentAccounting
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        public static MainWindow init;
         public MainWindow()
         {
             InitializeComponent();
+            init = this;
+            OpenPages(new Pages.LoginPage());
+        }
+        #region Оформление.
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Window_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (WindowState == WindowState.Maximized)
+                WindowState = WindowState.Normal;
+            else WindowState = WindowState.Maximized;
+        }
+        #endregion
+        public void OpenPages(Page page)
+        {
+            frame.Navigate(page);
         }
     }
 }

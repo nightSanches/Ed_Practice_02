ae8e4634cd2b074a8aaeff5b46bd1f0c5a91cf84
nightSanches <galesavich@gmail.com>
2026-01-31 06:16:33 +0500

    Создана модель Оборудование.

---
+++ Models/Equipment.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testEquipmentAccounting.Models
+{
+    public class Equipment
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Photo { get; set; }
+        public int? InventoryNumber { get; set; }
+        public int? RoomId { get; set; }
+        public int? ResponsibleUserId { get; set; }
+        public int? TempResponsibleUserId { get; set; }
+        public decimal? Cost { get; set; }
+        public int? DirectionId { get; set; }
+        public int? StatusId { get; set; }
+        public int? ModelId { get; set; }
+        public string? Comment { get; set; }
+    }
+}

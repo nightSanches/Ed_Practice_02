9f83a56f016a82bbb0290a95f68d3c96de191746
nightSanches <galesavich@gmail.com>
2026-01-31 06:07:48 +0500

    Создан класс UserSession для хранения данных пользователя

---
+++ Models/DropdownData.cs
@@ -0,0 +1,24 @@
+﻿namespace EquipmentAccounting.Models
+{
+    public class DropdownData
+    {
+        public List<DropdownItem> ConsumableTypes { get; set; } = new();
+        public List<DropdownItem> Users { get; set; } = new();
+        public List<DropdownItem> Consumables { get; set; } = new();
+        public List<DropdownItem> ConsumableCharacteristics { get; set; } = new();
+        public List<DropdownItem> Equipment { get; set; } = new();
+        public List<DropdownItem> Rooms { get; set; } = new();
+        public List<DropdownItem> Software { get; set; } = new();
+        public List<DropdownItem> Inventories { get; set; } = new();
+        public List<DropdownItem> EquipmentTypes { get; set; } = new();
+        public List<DropdownItem> Developers { get; set; } = new();
+        public List<DropdownItem> Statuses { get; set; } = new();
+        public List<DropdownItem> Directions { get; set; } = new();
+        public List<DropdownItem> Models { get; set; } = new();
+    }
+    public class DropdownItem
+    {
+        public int Id { get; set; }
+        public string DisplayText { get; set; } = string.Empty;
+    }
+}
---
+++ Models/UserSession.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquipmentAccounting.Models
+{
+    public static class UserSession
+    {
+        public static string Token { get; set; } = string.Empty;
+        public static string Role { get; set; } = string.Empty;
+        public static string FullName { get; set; } = string.Empty;
+        public static DropdownData DropdownData { get; set; } = new();
+        public static bool IsAuthenticated => !string.IsNullOrEmpty(Token);
+
+        public static void Clear()
+        {
+            Token = string.Empty;
+            Role = string.Empty;
+            FullName = string.Empty;
+            DropdownData = new DropdownData();
+        }
+    }
+}

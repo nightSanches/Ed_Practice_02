f8692d0aa88d93ef5af8bf84904a98a53cab935a
student-a502 <student-a502@STUD-RM3-A502>
2026-01-31 18:01:17 +0500

    Реализовано добавление фото к Оборудованию

--- Pages/Equipment/Add.xaml.cs
+++ Pages/Equipment/Add.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using EquipmentAccounting.Models;
 using EquipmentAccounting.Services;
+using Microsoft.Win32;
 
 namespace EquipmentAccounting.Pages.Equipment
 {
@@ -146,9 +148,13 @@ namespace EquipmentAccounting.Pages.Equipment
                         break;
                     }
                 }
+
+                if(equipment.Photo != null)
+                {
+                    LoadPhotoFromBase64();
+                }
             }
         }
-
         private void btnCancelClick(object sender, RoutedEventArgs e)
         {
             MainWindow.init.OpenPages(new Pages.Equipment.EquipmentPage());
@@ -159,14 +165,12 @@ namespace EquipmentAccounting.Pages.Equipment
             if(equipment == null)
             {
                 equipment = new Models.Equipment();
-
                 //обязательно для заполнения
                 equipment.Name = name.Text;
                 equipment.InventoryNumber = Convert.ToInt32(inventory_number.Text);
+                equipment.Photo = tempImageBase64;
 
-                //сделать добавление фото
-
-                if(!string.IsNullOrWhiteSpace(cost.Text))
+                if (!string.IsNullOrWhiteSpace(cost.Text))
                     equipment.Cost = Convert.ToDecimal(cost.Text);
                 else equipment.Cost = null;
 
@@ -212,8 +216,7 @@ namespace EquipmentAccounting.Pages.Equipment
                 //обязательно для заполнения
                 equipment.Name = name.Text;
                 equipment.InventoryNumber = Convert.ToInt32(inventory_number.Text);
-
-                //сделать добавление фото
+                equipment.Photo = tempImageBase64;
 
                 if (!string.IsNullOrWhiteSpace(cost.Text))
                     equipment.Cost = Convert.ToDecimal(cost.Text);
@@ -266,6 +269,105 @@ namespace EquipmentAccounting.Pages.Equipment
         private async void UpdateEquipment()
         {
             await _equipmentService.UpdateEquipmentAsync(equipment.Id, equipment);
+        }
+        private string tempImageBase64 = null;
+        private void btnLoadImage(object sender, RoutedEventArgs e)
+        {
+            tempImageBase64 = GetImageBase64FromDialog();
+            LoadPhotoFromBase64();
+        }
+
+        public static string GetImageBase64FromDialog()
+        {
+            try
+            {
+                var openFileDialog = new OpenFileDialog
+                {
+                    Title = "Выберите изображение(Максимум 16 Мб)",
+                    Filter = "Изображения (*.jpg; *.jpeg; *.png)|*.jpg;*.jpeg;*.png|Все файлы (*.*)|*.*",
+                    Multiselect = false
+                };
+
+                if (openFileDialog.ShowDialog() == true)
+                {
+                    string path = openFileDialog.FileName;
+                    if(!File.Exists(path))
+                    {
+                        throw new FileNotFoundException("Файл не найден", path);
+                    }
+
+                    var fileInfo = new FileInfo(path);
+
+                    if(fileInfo.Length > (16*1024*1024))
+                    {
+                        throw new InvalidOperationException($"Размер файла превышает максимально допустимый ({16*1024*1024} Мб)");
+                    }
+
+                    byte[] imageBytes = File.ReadAllBytes(path);
+                    string base64String =  Convert.ToBase64String(imageBytes);
+                    return base64String;
+                }
+                return null;
+            }
+            catch(Exception ex)
+            {
+                throw;
+            }
+        }
+        private void LoadPhotoFromBase64()
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(tempImageBase64))
+                {
+                    byte[] imageBytes = Convert.FromBase64String(tempImageBase64);
+
+                    using (MemoryStream memoryStream = new MemoryStream(imageBytes))
+                    {
+                        BitmapImage bitmapImage = new BitmapImage();
+                        bitmapImage.BeginInit();
+                        bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                        bitmapImage.StreamSource = memoryStream;
+                        bitmapImage.EndInit();
+                        bitmapImage.Freeze();
+
+                        imgPhoto.Source = bitmapImage;
+                    }
+                }
+                else if(!string.IsNullOrEmpty(equipment.Photo))
+                {
+                    byte[] imageBytes = Convert.FromBase64String(equipment.Photo);
+
+                    using (MemoryStream memoryStream = new MemoryStream(imageBytes))
+                    {
+                        BitmapImage bitmapImage = new BitmapImage();
+                        bitmapImage.BeginInit();
+                        bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                        bitmapImage.StreamSource = memoryStream;
+                        bitmapImage.EndInit();
+                        bitmapImage.Freeze();
+
+                        imgPhoto.Source = bitmapImage;
+                    }
+                }
+                else
+                {
+                    imgPhoto.Source = new BitmapImage(new Uri("/Images/default-equipment.png", UriKind.Relative));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка загрузки изображения: {ex.Message}");
+                imgPhoto.Source = new BitmapImage(new Uri("/Images/default-equipment.png", UriKind.Relative));
+            }
+        }
+        private void ImageDeleteClick(object sender, MouseButtonEventArgs e)
+        {
+            if(equipment != null)
+            {
+                equipment.Photo = null;
+                imgPhoto.Source = new BitmapImage(new Uri("/Images/default-equipment.png", UriKind.Relative));
+            }
         }
     }
 }

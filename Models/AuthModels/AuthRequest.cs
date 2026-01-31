7b08d6e9873d20639e3edd8d3a35faa7ee1b5f41
nightSanches <galesavich@gmail.com>
2026-01-31 06:15:47 +0500

    Создана модель для Авторизации

---
+++ Models/AuthModels/AuthRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace testEquipmentAccounting.Models.AuthModels
+{
+    public class AuthRequest
+    {
+        [JsonPropertyName("username")]
+        public string Username { get; set; } = string.Empty;
+
+        [JsonPropertyName("password")]
+        public string Password { get; set; } = string.Empty;
+    }
+}
---
+++ Models/AuthModels/AuthResponse.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace testEquipmentAccounting.Models.AuthModels
+{
+    public class AuthResponse
+    {
+        [JsonPropertyName("token")]
+        public string Token { get; set; } = string.Empty;
+
+        [JsonPropertyName("role")]
+        public string Role { get; set; } = string.Empty;
+
+        [JsonPropertyName("fullName")]
+        public string FullName { get; set; } = string.Empty;
+    }
+}

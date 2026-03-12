50f5b5820c297c0e54f8b2a3eb34eb1a4bcfadb3
nightSanches <galesavich@gmail.com>
2026-03-12 20:23:04 +0500

    Добавлены модели Статус и Тип оборудования

---
+++ Models/Status.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquipmentAccounting.Models
+{
+    public class Status
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}

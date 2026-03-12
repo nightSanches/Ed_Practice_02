d7f486e840904e13331e2aa6a332a0b09e5c7490
nightSanches <galesavich@gmail.com>
2026-03-12 21:14:46 +0500

    Модель для моделей оборудования

---
+++ Models/Model.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquipmentAccounting.Models
+{
+    public class Model
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int EquipmentTypeId { get; set; }
+    }
+}
